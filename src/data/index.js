568c07b04096835062548fc9672f7491bc77d236
ScorpionTaj <96800858+scorpionTaj@users.noreply.github.com>
2022-08-25 01:03:48 +0100

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,92 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'firstName1',
+    lastName: 'lastName1',
+    email: 'user1@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'firstName2',
+    lastName: 'lastName2',
+    email: 'user2@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'firstName3',
+    lastName: 'lastName3',
+    email: 'user3@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'firstName4',
+    lastName: 'lastName4',
+    email: 'user4@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'firstName5',
+    lastName: 'lastName5',
+    email: 'user5@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'firstName6',
+    lastName: 'lastName6',
+    email: 'user6@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'firstName7',
+    lastName: 'lastName7',
+    email: 'user7@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'firstName8',
+    lastName: 'lastName8',
+    email: 'user8@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'firstName9',
+    lastName: 'lastName9',
+    email: 'user9@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'firstName10',
+    lastName: 'lastName10',
+    email: 'user10@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  },
+  {
+    id:11,
+    firstName: 'Tajeddine',
+    lastName: 'Bourhim',
+    email: 'bourhimtajeddine@gmail.com',
+    salary:'1500',
+    date:'2022-08-25'
+  }
+];
+
+export { employeesData };
